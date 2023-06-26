baa3c5ae707cc4b711b01c47b618d162828f4522
Amala Hema <amalahema@gmail.com>
2023-06-26 16:12:09 -0400

    footer updated

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -72,8 +72,8 @@ function Portfolio() {
         <div className="project-summary">
           <h4>Workout Dashboard</h4>
           <p>
-            This project utilizes a Workout and Nutritional API to fetch data for a fitness-oriented application. The project is built on JavaScript and incorporates HTML and CSS/Bootstrap for designing and structuring the layout of the application. Additionally, two APIs are utilized to retrieve dynamic data, which can be manipulated using JavaScript to further customize the page.
-            <br />
+            This project utilizes a Workout and Nutritional API to fetch data for a fitness-oriented application. The project is built on JavaScript and incorporates HTML and CSS/Bootstrap for designing  the layout of the application.Two APIs are utilized to retrieve dynamic data, which can be manipulated using JavaScript to further customize the page.
+           
             Check out project demo <a href="https://drive.google.com/file/d/1khdhZMLiejKNgkrHiYcmd2QBcOD-JZNF/view"> here </a>
           </p>
         </div>
